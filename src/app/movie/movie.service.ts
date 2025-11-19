b70ede5ec93eda2d3f9a32f1e92c561118e62b95
Isabella Naranjo <i.naranjop@uniandes.edu.co>
2025-11-19 06:46:44 -0500

    Movie service

---
+++ src/app/movie/movie.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from './Movie';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+
+  private baseListUrl = 'http://157.253.205.147:8080/api/movies';
+  private baseDetailUrl = 'URL: http://157.253.205.147:8080/api/movies/'; //aca tocaria meterle el id
+
+  constructor(private http: HttpClient){} 
+
+  //metodo para leer peliculas
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.baseListUrl);
+  }
+
+  //metodo para el detail de una pelicula
+  getRecetaById(id: number): Observable<Movie> {
+    const url = `${this.baseDetailUrl}${id}`;
+    return this.http.get<Movie>(url);
+  }
+  
+  
+}
